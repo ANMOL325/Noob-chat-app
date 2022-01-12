86d025f3eb0b066fd6f1b51d812d4e15575b79c7
Anmol <mdmeharimam@gmail.com>
2022-01-12 18:51:10 +0530

    heorku host

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000; // Socket io Connection...
+const port2 = process.env.PORT || 8000;  // Listening to this Port.......
+
+const io = require('socket.io')(port, {
+    cors: {
+      origin: '*',
+    }
+  });
+
+const users = {};
+
+
+const static_path = path.join(__dirname, "");
+console.log(static_path);
+app.use(express.static(static_path));
+
+app.get("", (req,res) => {
+  res.render("index");
+}) 
+
+app.listen(port2, () => {
+  console.log(`Listening to ${port2}`);
+})
+
+
+io.on('connection', socket => {
+    socket.on('new-user-joined', name =>{
+        
+        console.log('New User', name);
+        users[socket.id] = name;
+        socket.broadcast.emit('user-joined', name);
+    })
+
+    socket.on('send', message =>{
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]})
+    });
+})
+
+
+
---
+++ js/Client.js
@@ -0,0 +1,42 @@
+const socket = io('http://localhost:3000');
+
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector('.Container');
+const Identity = document.getElementById("Identity");
+
+const name2 = prompt("Enter your name to join");
+
+Identity.innerText = `Profile:${name2} 👍`;
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`You: ${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = "";  
+})
+
+const append = (message, position)=>{
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('MessageRight');
+    messageContainer.append(messageElement);
+}
+socket.emit('new-user-joined', name2);
+
+socket.on('user-joined', name2 => {
+append(`${name2} joined the chat`, 'right');
+});
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left')
+})
+
+
+
+
+
+
+
+
+
