a87c94b808a85b4e88b8733b8bffdb79db4a6f14
Anmol <mdmeharimam@gmail.com>
2022-01-12 19:42:14 +0530

    heorku host

--- js/Client.js
+++ js/Client.js
@@ -1,4 +1,4 @@
-const socket = io();
+const socket = io('https://noob-chat-app.herokuapp.com');
 
 const form = document.getElementById('send-container');
 const messageInput = document.getElementById('messageInp');
